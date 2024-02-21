ddb98260ae2731446c77295dc14c895cd472b5c5
OpenJ92 <jacob.vartuli.92@gmail.com>
2024-02-21 18:33:51 -0500

    update db creation in server context. Fix names

--- database/create.py
+++ database/create.py
@@ -1,8 +1,11 @@
-from database.models.answers import Answers
-from database.models.questions import Questions
+from database.models.answers import Answer
+from database.models.questions import Question
 from database.models.surveys import Survey
+from database.models.survey_question_answer import Survey_Question_Answer
 
 from database.config import db
+from application.app import app
 
-if __name__ == "__main__":
-    db.create_all()
+def create_database():
+    with app.server.app_context():
+        db.create_all()
