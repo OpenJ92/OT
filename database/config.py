01a86c1059c901f66fc2891290e7d1f846abef4c
OpenJ92 <jacob.vartuli.92@gmail.com>
2024-02-21 18:33:06 -0500

    update database URI

--- database/config.py
+++ database/config.py
@@ -1,9 +1,7 @@
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 server = Flask(__name__)
-## Rebuild for installable form
-server.config[ "SQLALCHEMY_DATABASE_URI" ] = "sqlite:///ot.db"
+server.config[ "SQLALCHEMY_DATABASE_URI" ] = "sqlite:///instance/ot.db"
 server.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(server)
